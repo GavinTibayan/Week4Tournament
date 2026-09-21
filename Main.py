e8b0c3ba7a3f8ab7f4f8d66f82049f83fcdd0ac1
GavinTibayan <gavin.tibayan@stu.ocsb.ca>
2026-09-21 10:54:17 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,10 +9,17 @@
 
 # THIS IS WHERE YOU CODE
 #Team Name
-print("Whats the team name?)
+print("Whats the team name?")
+Team1 = input()
 #Wins
+Wins1 = input()
+Points1 = int(Wins1 * 2)
 #Losses
+Losses1 = input()
 #Ties
+Ties1 = input()
+Points2 = int(Ties1)
 #Calculate
+Points3 = int(Points1 + Points2)
 
-print("Team name",Team1,"wins:",Wins1,"Ties:",Ties1,"Losses:",Losses1,"Points:",Points1)
+print("Team name:",Team1,"wins:",Wins1,"Ties:",Ties1,"Losses:",Losses1,"Points:",Points3)
