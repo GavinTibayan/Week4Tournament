2cdc8db207af675742e713bbd6e2b3ed20b21c7a
GavinTibayan <gavin.tibayan@stu.ocsb.ca>
2026-09-21 11:13:45 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,17 +9,49 @@
 
 # THIS IS WHERE YOU CODE
 #Team Name
-print("Whats the team name?")
+print("\t\tList the names of 6 teams")
+print("Team 1:")
 Team1 = input()
+print("Team 2:")
+Team2 = input()
+print("Team 3:")
+Team3 = input()
+print("Team 4:")
+Team4 = input()
+print("Team 5:")
+Team5 = input()
+print("Team 6:")
+Team6 = input()
+
 #Wins
-Wins1 = input()
+print("\t\tHow many wins?")
+print(Team1)
+Wins1 = int(input())
 Points1 = int(Wins1 * 2)
 #Losses
-Losses1 = input()
+print("\t\tHow many losses?")
+Losses1 = int(input())
 #Ties
-Ties1 = input()
-Points2 = int(Ties1)
+print("\t\tHow many ties?")
+Ties1 = int(input())
+Points2 = int(Ties1 * 1)
 #Calculate
 Points3 = int(Points1 + Points2)
-
 print("Team name:",Team1,"wins:",Wins1,"Ties:",Ties1,"Losses:",Losses1,"Points:",Points3)
+#Podium Calculation
+
+
+#Podium
+print("In 6th place is...", Number6)
+print("\t\tpress enter to continue to the next lines")
+input()
+print("In 5th place is...", Number5)
+input()
+print("In 4th place is...", Number4)
+input()
+print("In 3rd place is...", Number3)
+input()
+print("In 2nd place is...", Number2)
+input()
+print("And the winner of this tournament is...", Number1)
+
