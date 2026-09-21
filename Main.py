58502c27b911d1d9c5e7f889799aee40e7742719
GavinTibayan <gavin.tibayan@stu.ocsb.ca>
2026-09-21 10:46:46 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,3 +8,11 @@
 #############################################
 
 # THIS IS WHERE YOU CODE
+#Team Name
+print("Whats the team name?)
+#Wins
+#Losses
+#Ties
+#Calculate
+
+print("Team name",Team1,"wins:",Wins1,"Ties:",Ties1,"Losses:",Losses1,"Points:",Points1)
